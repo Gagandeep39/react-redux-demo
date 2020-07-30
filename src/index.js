7ab6b1b08afd20a022b693090d32c2de58f537de
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-07-30 15:02:21 +0530

    Added Middleware

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import counterReducer from './store/reducers/counter';
 import resultReducer from './store/reducers/result';
@@ -14,7 +14,23 @@ const rootReducer = combineReducers({
   ctr: counterReducer,
   res: resultReducer
 })
-const store = createStore(rootReducer);
+
+const logger = (store) => {
+  return next => {
+    return action => {
+      console.log('[Middleware] Dispatching', action);
+      // Ensures dispatcher to execute action in reducer
+      const result = next(action);
+      console.log('[Middleware next state]', store.getState());
+      return result;
+    }
+  }
+}
+
+// Compose allows combining of middleware, simila to combine reducer which combine reducer
+// 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
 
 ReactDOM.render(
   <React.StrictMode>
