7f5620f39447f30aa50df52655cab8b1689d332c
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-07-30 17:57:19 +0530

    Created index file to manage action export

---
+++ src/store/actions/index.js
@@ -0,0 +1,12 @@
+// Export can be used to coolect exports from multiple files and export it from one place
+export {
+  increment,
+  decrement,
+  add,
+  sub
+} from './counter';
+
+export {
+  storeResult,
+  deleteResult
+} from './result';
