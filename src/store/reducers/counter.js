1183c60a36341ac5e680ebf1877339fa517f8904
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-07-30 18:34:57 +0530

    Moved reducer logic to utility function
    Refer the commented code for reliability

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,37 +1,52 @@
 import * as actionTypes from '../actions/actionsTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
-  counter: 0
-}
+  counter: 0,
+};
 
 export default function counterReducer(state = initialState, action) {
   switch (action.type) {
     case actionTypes.INCREMENT:
-      // Different from constNewState = state as it creates a clone instead of providing referece
-      // We must always make a copy i.e update objects immutably
-      // const newState = Object.assign({}, state);
-      // newState.counter = state.counter + 1
-      // return newState;
-      // ******* OR *****
-      return {
-        ...state,
-        counter: state.counter + 1
-      }
+      return updateObject(state, { counter: state.counter + 1 });
     case actionTypes.DECREMENT:
-      return {
-        ...state,
-        counter: state.counter - 1
-      }
+      return updateObject(state, { counter: state.counter - 1 });
     case actionTypes.ADD:
-      return {
-        ...state,
-        counter: state.counter + action.value
-      }
+      return updateObject(state, { counter: state.counter + action.value });
     case actionTypes.SUBSTRACT:
-      return {
-        ...state,
-        counter: state.counter -action.value
-      }
+      return updateObject(state, { counter: state.counter - action.value });
   }
-    return state;
+  return state;
 }
+
+// export default function counterReducer(state = initialState, action) {
+//   switch (action.type) {
+//     case actionTypes.INCREMENT:
+//       // Different from constNewState = state as it creates a clone instead of providing referece
+//       // We must always make a copy i.e update objects immutably
+//       // const newState = Object.assign({}, state);
+//       // newState.counter = state.counter + 1
+//       // return newState;
+//       // ******* OR *****
+//       return {
+//         ...state,
+//         counter: state.counter + 1
+//       }
+//     case actionTypes.DECREMENT:
+//       return {
+//         ...state,
+//         counter: state.counter - 1
+//       }
+//     case actionTypes.ADD:
+//       return {
+//         ...state,
+//         counter: state.counter + action.value
+//       }
+//     case actionTypes.SUBSTRACT:
+//       return {
+//         ...state,
+//         counter: state.counter -action.value
+//       }
+//   }
+//     return state;
+// }
--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionsTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
   results: []
@@ -7,17 +8,29 @@ const initialState = {
 export default function resultReducer(state = initialState, action) {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
-      return {
-        ...state,
-        results: state.results.concat({id: new Date(), value: action.result}) // taking value as param on calling
-        // results: state.results.concat({id: new Date(), value: state.counter}) // Substitue of.push method as this is immutable(Creates copy of array)
-      }
+      return updateObject(...state, state.results.concat({id: new Date(), value: action.result}))
     case actionTypes.DELETE_RESULT:
       const updatedArray = state.results.filter(results => results.id != action.elementId)
-      return {
-        ...state,
-        results: updatedArray
-      }
+      return updateObject(...state,{results: updatedArray})
   }
     return state;
 }
+
+
+// export default function resultReducer(state = initialState, action) {
+//   switch (action.type) {
+//     case actionTypes.STORE_RESULT:
+//       return {
+//         ...state,
+//         results: state.results.concat({id: new Date(), value: action.result}) // taking value as param on calling
+//         // results: state.results.concat({id: new Date(), value: state.counter}) // Substitue of.push method as this is immutable(Creates copy of array)
+//       }
+//     case actionTypes.DELETE_RESULT:
+//       const updatedArray = state.results.filter(results => results.id != action.elementId)
+//       return {
+//         ...state,
+//         results: updatedArray
+//       }
+//   }
+//     return state;
+// }
