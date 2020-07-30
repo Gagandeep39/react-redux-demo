760a8088209ac5445ee6fffeb54b1c4a49a0cd90
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-07-30 18:47:54 +0530

    Made a leaner switch case

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -2,20 +2,31 @@ import * as actionTypes from '../actions/actionsTypes';
 import { updateObject } from '../utility';
 
 const initialState = {
-  results: []
-}
+  results: [],
+};
+
+const storeResult = (state, action) => {
+  return updateObject(state, {
+    results: state.results.concat({ id: new Date(), value: action.result }),
+  });
+};
+
+const deleteResult = (state, action) => {
+  const updatedArray = state.results.filter(
+    (results) => results.id != action.elementId
+  );
+  return updateObject(state, { results: updatedArray });
+};
 
 export default function resultReducer(state = initialState, action) {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
-      return updateObject(...state, state.results.concat({id: new Date(), value: action.result}))
+      return storeResult(state, action);
     case actionTypes.DELETE_RESULT:
-      const updatedArray = state.results.filter(results => results.id != action.elementId)
-      return updateObject(...state,{results: updatedArray})
+      return deleteResult(state, action);
   }
-    return state;
+  return state;
 }
-
 
 // export default function resultReducer(state = initialState, action) {
 //   switch (action.type) {
